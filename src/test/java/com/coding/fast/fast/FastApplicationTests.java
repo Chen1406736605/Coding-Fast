88b26e2280de5d37e25236bbe8bd8467cb5aab56
陈智聪 <1406736605@qq.com>
2021-02-18 16:38:14 +0800

    调整项目结构

---
+++ src/test/java/com/coding/fast/fast/FastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding.fast.fast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
